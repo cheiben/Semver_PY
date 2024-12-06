c22c46850162c521bdbf97f57d37e48f7676daf4
cheiben <chei610@icloud.com>
2024-12-05 23:25:43 -0500

    adding file

---
+++ install-hooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing Git hooks..."
+cp -r hooks/* .git/hooks/
+chmod +x .git/hooks/*
+echo "Git hooks installed successfully."
+
+
+## Setting Up Git Hooks
+
+#After cloning the repository, run the following command to set up Git hooks:
+
+#```bash
+# ./install-hooks.sh ```
